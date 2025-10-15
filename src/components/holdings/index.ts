cba365dd8e230cc71d8a05507dca979e45601256
niyyzf <niyyzf@hotmail.com>
2025-10-15 14:46:11 +0800

    去处为使用代码

--- src/components/holdings/index.ts
+++ src/components/holdings/index.ts
@@ -6,7 +6,6 @@ export { StatisticsCards } from './hold-statistics-cards';
 export { Toolbar } from './hold-toolbar';
 export { FilterBar } from './hold-filter-bar';
 export { TableView } from './hold-table-view';
-export { CompactView } from './hold-compact-view';
 export { ViewModeDock } from './hold-view-mode-dock';
 export { StockInfoCard } from './hold-stock-info-card';
 export { default as CountUp } from './hold-count-up';
